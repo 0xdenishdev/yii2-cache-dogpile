7a0700ebdc766c4ef1b3ddbc0384867ce2f46072
denish <denishorobtsov@gmail.com>
2017-04-02 23:12:17 +0300

    Added component stub.

---
+++ src/Dogpile/MemCache.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hexspeak\Dogpile;
+
+use yii\caching\MemCache as AncestorMemCache;
+
+/**
+ * Class MemCache.
+ *
+ * @package Hexspeak\Dogpile
+ */
+class MemCache extends AncestorMemCache
+{
+
+}
