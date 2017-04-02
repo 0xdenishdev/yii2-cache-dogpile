c837955b2f046c6b4d468d4171c13211a3357f97
denish <denishorobtsov@gmail.com>
2017-04-02 22:37:31 +0300

    Initial commit.

---
+++ src/Dogpile/Module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hexspeak\Dogpile;
+
+use yii\base\Module as BaseModule;
+
+class Module extends BaseModule
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
