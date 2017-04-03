58098de502c2e3fc2ac36de56a9f5bdf1baa471e
denish <denish@templatemonster.me>
2017-04-03 10:44:05 +0300

    Changed component description and fixed class name respectively.

---
+++ src/Dogpile/Cache.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hexspeak\Dogpile;
+
+use yii\base\Component;
+
+/**
+ * Class Cache.
+ * Component designed for solving dogpile cache issues.
+ *
+ * @package Hexspeak\Dogpile
+ */
+class Cache extends Component
+{
+
+}
