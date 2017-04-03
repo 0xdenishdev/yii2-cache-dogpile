b6f211764e9f29e93f9abd644e095246b683479a
denish <denish@templatemonster.me>
2017-04-03 10:51:39 +0300

    Added stub for storage interface.

---
+++ src/Dogpile/Storage/StorageInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hexspeak\Dogpile\Storage;
+
+/**
+ * Interface StorageInterface.
+ * Describes an interface which should be implemented by specific cache storage.
+ *
+ * @package Hexspeak\Dogpile\Storage
+ */
+interface StorageInterface
+{
+
+}
